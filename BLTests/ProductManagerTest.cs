e08d2ad0f84b555db65716d75105eb05bd502985
MaxSter1 <63198631+MaxSter1@users.noreply.github.com>
2020-11-12 13:07:44 +0200

    Add files via upload

---
+++ BLTests/ProductManagerTest.cs
@@ -0,0 +1,56 @@
+﻿using BusinessLogic.Concrete;
+using DAL.Interface;
+using DTO;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLTests
+{
+    [TestFixture]
+    public class ProductManagerTest
+    {
+        private Mock<IProductDal> productDal;
+        private ProductManager manager;
+
+        [SetUp]
+        public void Setup()
+        {
+            productDal = new Mock<IProductDal>(MockBehavior.Strict);
+
+            manager = new ProductManager(productDal.Object);
+        }
+
+        [Test]
+        public void GetAllProductsTest()
+        {
+            var productList = new List<ProductDTO>() { new ProductDTO { Id = 1, CategoryId = 2, EntryPrice = 100, IsLocked = false, Name = "Product", SellingPrice = 148 } };
+            productDal.Setup(p => p.GetAllProducts()).Returns(productList);
+            var res = manager.GetAllProducts();
+            NUnit.Framework.Assert.IsTrue(res.Count > 0);
+        }
+        [Test]
+        public void GetProductByIdTest()
+        {
+            int id = 1;
+            var product = new ProductDTO { Id = id, CategoryId = 2, EntryPrice = 100, IsLocked = false, Name = "Product", SellingPrice = 148 };
+            productDal.Setup(p => p.GetProductById(id)).Returns(product);
+            var res = manager.GetProductById(id);
+
+            NUnit.Framework.Assert.AreEqual(res.Id,id);
+        }
+        [Test]
+        public void UpdateProductTest()
+        {
+            var product = new ProductDTO { Id = 1, CategoryId = 2, EntryPrice = 100, IsLocked = false, Name = "Product", SellingPrice = 148 };
+            var UpdatedProduct = product;
+            productDal.Setup(p => p.UpdateProduct(product)).Returns(UpdatedProduct);
+            var res = manager.UpdateProduct(product);
+            NUnit.Framework.Assert.AreEqual(res,UpdatedProduct);
+        }
+    }
+}
---
+++ BLTests/RoleManagerTest.cs
@@ -0,0 +1,38 @@
+﻿using BusinessLogic.Concrete;
+using DAL;
+using DAL.Interface;
+using DTO;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLTests
+{
+    [TestFixture]
+    public class RoleManagerTest
+    {
+        private Mock<IRoleDal> roleDal;
+        private RoleManager manager;
+
+        [SetUp]
+        public void Setup()
+        {
+            roleDal = new Mock<IRoleDal>(MockBehavior.Strict);
+
+            manager = new RoleManager(roleDal.Object);
+        }
+
+        [Test]
+        public void GetAllRolesTest()
+        {
+            var roleList = new List<RoleDTO>() { new RoleDTO {Id = 1,Description = "description",Name = "name" } };
+            roleDal.Setup(p => p.GetAllRoles()).Returns(roleList);
+            var res = manager.GetAllRoles();
+            NUnit.Framework.Assert.IsTrue(res.Count >0);
+        }
+    }
+}
