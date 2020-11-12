b67be6f830773fbe068860e1f89346091d71b883
MaxSter1 <63198631+MaxSter1@users.noreply.github.com>
2020-11-12 13:06:42 +0200

    Add files via upload

---
+++ WFApp/LoginForm.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace WFApp
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Password = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Login = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Login";
+            // 
+            // Password
+            // 
+            this.Password.Location = new System.Drawing.Point(90, 87);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(100, 20);
+            this.Password.TabIndex = 1;
+            this.Password.UseSystemPasswordChar = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password";
+            // 
+            // Login
+            // 
+            this.Login.Location = new System.Drawing.Point(90, 25);
+            this.Login.Name = "Login";
+            this.Login.Size = new System.Drawing.Size(100, 20);
+            this.Login.TabIndex = 0;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(221, 138);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 2;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(308, 173);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.Login);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.label1);
+            this.Name = "LoginForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Password;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Login;
+        private System.Windows.Forms.Button btnLogin;
+    }
+}
+
---
+++ WFApp/LoginForm.cs
@@ -0,0 +1,40 @@
+﻿using BusinessLogic.Interface;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFApp
+{
+    public partial class LoginForm : Form
+    {
+        private readonly IAuthManager _authManager;
+        public LoginForm(IAuthManager authManager)
+        {
+            _authManager = authManager;
+            InitializeComponent();
+        }
+
+        private void DoLogin()
+        {
+            if(_authManager.Login(Login.Text,Password.Text))
+            {
+                DialogResult = DialogResult.OK;
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Invalid login or password");
+            }
+        }
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            DoLogin();
+        }
+    }
+}
