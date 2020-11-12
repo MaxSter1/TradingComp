9054ce0125bad7c775eb9387a7d8882665a1a5e9
MaxSter1 <63198631+MaxSter1@users.noreply.github.com>
2020-11-12 13:02:33 +0200

    Add files via upload

---
+++ BLTests/AuthManagerTest.cs
@@ -0,0 +1,39 @@
+﻿using BusinessLogic.Concrete;
+using DAL.Concrete;
+using DAL.Interface;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLTests
+{
+    [TestFixture]
+    public class AuthManagerTest
+    {
+        private Mock<IUserDal> userDal;
+        private AuthManager manager;
+
+        [SetUp]
+        public void Setup()
+        {
+            userDal = new Mock<IUserDal>(MockBehavior.Strict);
+
+            manager = new AuthManager(userDal.Object);
+        }
+
+        [Test]
+        public void LoginTest()
+        {
+            string login = "login";
+            string pass = "password";
+            userDal.Setup(p => p.Login(login, pass)).Returns(true);
+            var res = manager.Login(login, pass);
+            NUnit.Framework.Assert.IsTrue(res);
+        }
+
+    }
+}
---
+++ BLTests/CategoryManagerTest.cs
@@ -0,0 +1,38 @@
+﻿using BusinessLogic.Concrete;
+using DAL.Interface;
+using DTO;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLTests
+{
+    [TestFixture]
+    public class CategoryManagerTest
+    {
+        private Mock<ICategoryDal> categoryDal;
+        private CategoryManager manager;
+
+        [SetUp]
+        public void Setup()
+        {
+            categoryDal = new Mock<ICategoryDal>(MockBehavior.Strict);
+
+            manager = new CategoryManager(categoryDal.Object);
+        }
+
+        [Test]
+        public void GetAllCategoriesTest()
+        {
+            var categoryList = new List<CategoryDTO>() { new CategoryDTO { Id = 1, Name = "category1" }, new CategoryDTO { Id = 2, Name = "category2" } };
+            categoryDal.Setup(p => p.GetAllCategories()).Returns(categoryList);
+            var res = manager.GetAllCategories();
+            NUnit.Framework.Assert.IsTrue(res.Count>0);
+        }
+
+    }
+}
